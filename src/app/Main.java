99b0553655fb2817a02947866b20894984c0ebf0
Suraj Mohajkar <surajmohajkar@gmail.com>
2026-06-30 21:21:23 +0530

    LMS-001 Initial project setup

---
+++ src/app/Main.java
@@ -0,0 +1,11 @@
+package app;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("=======================================");
+        System.out.println("   LIBRARY MANAGEMENT SYSTEM");
+        System.out.println("=======================================");
+        System.out.println();
+        System.out.println("Project initialized successfully.");
+    }
+}
