4ea1885ab6e7929709ecdd1de8033dfda84f4bfd
Suraj Mohajkar <surajmohajkar@gmail.com>
2026-07-20 20:43:07 +0530

    RFE-004 Extract utility classes

--- src/util/ConsoleUtil.java
+++ src/util/ConsoleUtil.java
@@ -1,4 +1,22 @@
 package util;
 
-public class ConsoleUtil {
+public final class ConsoleUtil {
+    private ConsoleUtil(){
+
+    }
+    public static void printHeader(String title){
+        System.out.println();
+        System.out.println("==================================");
+        System.out.println(title);
+        System.out.println("==================================");
+    }
+    public static void printSuccess(String message){
+        System.out.println("[SUCCESS] " + message);
+    }
+    public static void printError(String message){
+        System.out.println("[ERROR] " + message);
+    }
+    public static void printLine(){
+        System.out.println("----------------------------------------------");
+    }
 }
